709eacfde2e9ff0c0073c3f440cbf52682e52122
Luka-Magic <stargazinger0928@gmail.com>
2022-04-23 12:03:15 +0900

    update

---
+++ exp/exp_0005/env_wrapper.py
@@ -0,0 +1,73 @@
+import numpy as np
+import torch
+from torchvision import transforms
+import gym
+from gym.spaces import Box
+from gym.wrappers import FrameStack, RecordVideo
+from nes_py.wrappers import JoypadSpace
+
+
+class SkipFrame(gym.Wrapper):
+    def __init__(self, env, skip):
+        super().__init__(env)
+        self._skip = skip
+
+    def step(self, action):
+        total_reward = 0.0
+        done = False
+        for i in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            total_reward += reward
+            if done:
+                break
+        return obs, total_reward, done, info
+
+
+class GrayScaleObservation(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        obs_shape = self.observation_space.shape[:2]
+        self.observation_space = Box(
+            low=0, high=255, shape=obs_shape, dtype=np.uint8)
+
+    def permute_orientation(self, observation):
+        observation = np.transpose(observation, (2, 0, 1))
+        observation = torch.tensor(observation.copy(), dtype=torch.float)
+        return observation
+
+    def observation(self, observation):
+        observation = self.permute_orientation(observation)
+        transform = transforms.Grayscale()
+        observation = transform(observation)
+        return observation
+
+
+class ResizeObservation(gym.ObservationWrapper):
+    def __init__(self, env, shape):
+        super().__init__(env)
+        if isinstance(shape, int):
+            self.shape = (shape, shape)
+        else:
+            self.shape = tuple(shape)
+        obs_shape = self.shape + self.observation_space.shape[2:]
+        self.observation_space = Box(
+            low=0, high=255, shape=obs_shape, dtype=np.uint8)
+
+    def observation(self, observation):
+        transforms_ = transforms.Compose([
+            transforms.Resize(self.shape),
+            transforms.Normalize(0, 255)
+        ])
+        observation = transforms_(observation).squeeze(0)
+        return observation
+
+
+def all_wrapper(env, cfg, save_dir, init_episode):
+    env = JoypadSpace(env, cfg.actions)
+    env = RecordVideo(env, video_folder=save_dir / 'video')
+    env.episode_id = init_episode
+    env = SkipFrame(env, skip=cfg.state_skip)
+    env = GrayScaleObservation(env)
+    env = ResizeObservation(env, shape=(cfg.state_height, cfg.state_width))
+    env = FrameStack(env, num_stack=cfg.state_channel)
+    return env
---
+++ exp/exp_0005/main.py
@@ -0,0 +1,51 @@
+import os
+import hydra
+import wandb
+from pathlib import Path
+from omegaconf import DictConfig
+from tqdm.notebook import tqdm
+# 環境
+import gym_super_mario_bros
+from env_wrapper import all_wrapper
+# エージェント
+from agent import Mario
+
+
+@hydra.main(config_path='config', config_name='config')
+def main(cfg: DictConfig):
+    # 設定
+    save_dir = Path('/'.join(os.getcwd().split('/')
+                    [:-6])) / f"outputs/{os.getcwd().split('/')[-4]}"
+    save_dir.mkdir(exist_ok=True)
+
+    # wandb
+    if cfg.wandb:
+        wandb.login()
+        wandb.init(project=cfg.wandb_project, entity='luka-magic',
+                   name=os.getcwd().split('/')[-4], config=cfg)
+
+    # 環境
+    env = gym_super_mario_bros.make(cfg.environment)
+
+    # エージェント
+    mario = Mario(cfg, action_dim=env.action_space.n, save_dir=save_dir)
+    init_episode = mario.restart_episodes
+    env = all_wrapper(env, cfg, save_dir, init_episode)
+
+    # 学習
+    for episode in tqdm(range(init_episode, cfg.episodes)):
+        state = env.reset()
+        while True:
+            action = mario.action(state)
+            next_state, reward, done, info = env.step(action)
+            mario.push(state, next_state, action, reward, done)
+            mario.learn()
+            state = next_state
+
+            if done or info['flag_get']:
+                break
+        mario.log_episode(episode, info)
+
+
+if __name__ == '__main__':
+    main()
