fc3323dd482c617f12378f0d2707b58214b7e6f6
Luka-Magic <stargazinger0928@gmail.com>
2022-04-26 09:04:52 +0900

    update

--- exp/exp_0007/agent.py
+++ exp/exp_0007/agent.py
@@ -119,7 +119,7 @@ class Mario:
         exp = self.Transition(state, next_state, action, reward, done)
         self.multi_step_trainsitions.append(exp)
         # next_state = exp.next_state
-        
+
         if len(self.multi_step_trainsitions) < self.multi_step_num * self.state_channel:
             return
         multi_step_reward = 0
@@ -135,7 +135,6 @@ class Mario:
                 state, _, action, _, _ = self.multi_step_trainsitions.popleft()
             else:
                 _ = self.multi_step_trainsitions.popleft()
-        print(len(self.multi_step_trainsitions))
         exp = self.Transition(state, exp.next_state, action,
                               multi_step_reward, exp.done)
 
