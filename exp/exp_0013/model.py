ac3effe98977644ab1042cf180737478bbe029be
Luka-Magic <stargazinger0928@gmail.com>
2022-04-29 23:13:27 +0900

    update

---
+++ exp/exp_0013/model.py
@@ -0,0 +1,85 @@
+import math
+import torch
+from torch import nn
+import torch.nn.functional as F
+import copy
+
+
+class FactorizedNoisy(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(FactorizedNoisy, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+        # 学習パラメータを生成
+        self.u_w = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.sigma_w = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.u_b = nn.Parameter(torch.Tensor(out_features))
+        self.sigma_b = nn.Parameter(torch.Tensor(out_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        # 初期値設定
+        stdv = 1. / math.sqrt(self.u_w.size(1))
+        self.u_w.data.uniform_(-stdv, stdv)
+        self.u_b.data.uniform_(-stdv, stdv)
+
+        initial_sigma = 0.5 * stdv
+        self.sigma_w.data.fill_(initial_sigma)
+        self.sigma_b.data.fill_(initial_sigma)
+
+    def forward(self, x):
+        # 毎回乱数を生成
+        rand_in = self._f(torch.randn(
+            1, self.in_features, device=self.u_w.device))
+        rand_out = self._f(torch.randn(
+            self.out_features, 1, device=self.u_w.device))
+        epsilon_w = torch.matmul(rand_out, rand_in)
+        epsilon_b = rand_out.squeeze()
+
+        w = self.u_w + self.sigma_w * epsilon_w
+        b = self.u_b + self.sigma_b * epsilon_b
+        return F.linear(x, w, b)
+
+    def _f(self, x):
+        return torch.sign(x) * torch.sqrt(torch.abs(x))
+
+
+
+class MarioNet(nn.Module):
+    def __init__(self, cfg, input_dim, output_dim):
+        super().__init__()
+        c, h, w = input_dim
+        if h != cfg.state_height:
+            raise ValueError(f'Expecting input height: 84, got: {h}')
+        if w != cfg.state_width:
+            raise ValueError(f'Expecting input width: 84, got: {w}')
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=c, out_channels=32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64,
+                      kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64,
+                      kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+        self.values = nn.Sequential(
+            FactorizedNoisy(3136, 512),
+            nn.ReLU(),
+            FactorizedNoisy(512, 1)
+        )
+        self.advantages = nn.Sequential(
+            FactorizedNoisy(3136, 512),
+            nn.ReLU(),
+            FactorizedNoisy(512, output_dim)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.size(0), -1)
+        values = self.values(x)
+        advantages = self.advantages(x)
+        q = values + (advantages - advantages.mean(dim=1, keepdims=True))
+        return q
---
+++ exp/exp_0013/utils/customwrappers.py
@@ -0,0 +1,78 @@
+from gym import logger
+import numpy as np
+import os.path
+import os
+from typing import Callable
+import gc
+
+import gym
+from gym import logger
+
+
+def capped_cubic_video_schedule(episode_id, video_save_interval):  # 条件
+    return episode_id % video_save_interval == 0
+
+
+class CustomRecordVideo(gym.Wrapper):
+    def __init__(
+        self,
+        env,
+        cfg,
+        init_episode=0,
+        episode_trigger: Callable[[int], bool] = None,
+    ):
+        super().__init__(env)
+        if episode_trigger is None:
+            episode_trigger = capped_cubic_video_schedule
+
+        self.video_save_interval = cfg.video_save_interval
+        self.episode_trigger = episode_trigger
+
+        self.recording = False
+        self.episode_id = init_episode
+        self.frames = []
+
+    def reset(self, **kwargs):
+        observations = super().reset(**kwargs)
+        if not self.recording and self._video_enabled():
+            self.start_video_recorder()
+            del self.frames
+            gc.collect()
+            self.frames = []
+            frame = observations.copy()
+            self.frames.append(frame)
+        return observations
+
+    def start_video_recorder(self):
+        self.recording = True
+
+    def _video_enabled(self):
+        return self.episode_trigger(self.episode_id, self.video_save_interval)
+
+    def step(self, action):
+        observations, rewards, dones, infos = super().step(action)
+
+        dones = bool(dones)
+        video = None
+        if infos['flag_get']:
+            dones = True
+
+        if dones:
+            self.episode_id += 1
+
+        if self.recording:
+            frame = observations.copy()
+            self.frames.append(frame)
+            if dones:
+                video = self.close_video_recorder()
+
+        infos['video'] = video
+        return observations, rewards, dones, infos
+
+    def close_video_recorder(self) -> None:
+        self.recording = False
+        if len(self.frames):
+            video = np.stack(self.frames).transpose(0, 3, 1, 2)
+            return video
+        else:
+            return None
